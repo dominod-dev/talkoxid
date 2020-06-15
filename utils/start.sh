f6fd970ad3897a32b8f4f4475b0201f99c769f0b
Dominod <dev@dominod.fr>
2020-06-15 13:47:48 +0200

    Use a working docker-compose

--- utils/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker run --rm --name db -v $PWD/data:/data/db -d mongo:4.0 --smallfiles --replSet rs0 --oplogSize 128
-sleep 5
-docker exec -ti db mongo --eval "printjson(rs.initiate())"
-docker run --rm --name rocketchat -p 3000:3000 --link db --env MONGO_OPLOG_URL=mongodb://db:27017/local -d rocket.chat
