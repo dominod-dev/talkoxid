11d4f3d17c0a58f002312f2d0522d8fd0c197817
Dominod <dev@dominod.fr>
2020-08-11 22:47:16 +0200

    Add sound to notification
    For now the freedesktop instant message sound is hardcoded

--- src/notifications/mod.rs
+++ src/notifications/mod.rs
@@ -1,5 +1,8 @@
 use super::core::Notification;
+use rodio::Source;
 use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
 
 pub struct DesktopNotifier;
 
@@ -9,6 +12,14 @@ impl Notification for DesktopNotifier {
             .summary(&title[..])
             .body(&content[..])
             .show()?;
+        let device = rodio::default_output_device().unwrap();
+
+        let file = File::open("/usr/share/sounds/freedesktop/stereo/message-new-instant.oga")?;
+        let source = rodio::Decoder::new(BufReader::new(file)).unwrap_or_else(|err| {
+            log::info!("{:?}", err);
+            panic!();
+        });
+        rodio::play_raw(&device, source.convert_samples());
         Ok(())
     }
 }
